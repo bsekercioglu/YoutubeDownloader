2f10da525cd7566fb40f186091426d0f0213a3ff
bsekercioglu <bsekercioglu@gmail.com>
2022-01-06 09:52:25 +0300

    kurulum

---
+++ YoutubeDownloader/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace YoutubeDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.lblMesaj = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Youtube URL";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtUrl.Location = new System.Drawing.Point(106, 22);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(446, 26);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(471, 54);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(81, 28);
+            this.btnDownload.TabIndex = 2;
+            this.btnDownload.Text = "Video İndir";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(449, 6);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(103, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "www.sekercioglu.eu";
+            // 
+            // lblMesaj
+            // 
+            this.lblMesaj.AutoSize = true;
+            this.lblMesaj.Location = new System.Drawing.Point(28, 69);
+            this.lblMesaj.Name = "lblMesaj";
+            this.lblMesaj.Size = new System.Drawing.Size(0, 13);
+            this.lblMesaj.TabIndex = 5;
+            this.lblMesaj.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblMesaj_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 93);
+            this.Controls.Add(this.lblMesaj);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sekercioglu.eu | Youtube Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel lblMesaj;
+    }
+}
+
---
+++ YoutubeDownloader/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoLibrary;
+
+namespace YoutubeDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DownloadVideo(string id)
+        {
+            bool s = true;
+            using (var service = Client.For(YouTube.Default))
+            {
+                while (s)
+                {
+                    lblMesaj.Enabled = false;   
+                    lblMesaj.Text="Yükleniyor...";
+
+                    var video = service.GetVideo("https://youtube.com/watch?v=" + id);
+                    
+                    string folder = GetDefaultFolder();
+
+                    string path = Path.Combine(folder, video.FullName);
+
+                   lblMesaj.Text="Kaydediliyor...";
+
+                    File.WriteAllBytes(path, video.GetBytes());
+
+                    lblMesaj.Text = "Tamamlandı";
+                    lblMesaj.Enabled = true;
+                    s = false;
+                }
+            }
+        }
+        static string GetDefaultFolder()
+        {
+            var home = Environment.GetFolderPath(
+                Environment.SpecialFolder.UserProfile);
+
+            return Path.Combine(home, "Downloads");
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text.Trim().Replace("www.","").Replace("https://youtube.com/watch?v=", "");
+
+                DownloadVideo(url);
+            
+        }
+
+        private void lblMesaj_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", GetDefaultFolder());
+        }
+    }
+}
